d7668b35b61ce5d447b9f203ce1757e7d844437d
Luiz Leite Oliveira <luiz.leite@hotmart.com>
2020-02-11 10:23:53 -0300

    create getfeatures for all features on FeatureController.kt

--- api/src/main/kotlin/com/luizleiteoliveira/abtest/rest/FeatureController.kt
+++ api/src/main/kotlin/com/luizleiteoliveira/abtest/rest/FeatureController.kt
@@ -1,11 +1,19 @@
 package com.luizleiteoliveira.abtest.rest
 
+import com.luizleiteoliveira.abtest.FeatureRepository
+import com.luizleiteoliveira.abtest.entity.Feature
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.support.Repositories
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class FeatureController {
+@RequestMapping("/features")
+class FeatureController(private val featureRepository: FeatureRepository) {
 
-    @GetMapping("/features")
-    fun getFeatures() = "teste"
+    @GetMapping
+    fun getFeatures(): List<Feature> {
+        return featureRepository.findAll().toList()
+    }
 }
