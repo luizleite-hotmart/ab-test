e1d65409a378c33b9b31f76139b7b89d24079fb2
Luiz Leite Oliveira <luiz.leite@hotmart.com>
2020-02-11 10:41:22 -0300

    create all calls from our feature crud

--- api/src/main/kotlin/com/luizleiteoliveira/abtest/rest/FeatureController.kt
+++ api/src/main/kotlin/com/luizleiteoliveira/abtest/rest/FeatureController.kt
@@ -4,9 +4,8 @@ import com.luizleiteoliveira.abtest.FeatureRepository
 import com.luizleiteoliveira.abtest.entity.Feature
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.support.Repositories
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.websocket.server.PathParam
 
 @RestController
 @RequestMapping("/features")
@@ -15,5 +14,23 @@ class FeatureController(private val featureRepository: FeatureRepository) {
     @GetMapping
     fun getFeatures(): List<Feature> {
         return featureRepository.findAll().toList()
+    }
+
+    @PostMapping
+    fun createFeature(@RequestBody feature: Feature): Long {
+        featureRepository.save(feature)
+        return feature.id
+    }
+
+    @PutMapping
+    fun updateFeature(@RequestBody feature: Feature): Long {
+        featureRepository.save(feature)
+        return feature.id
+    }
+
+    @GetMapping("/{featureId}")
+    fun checkUserAbleToUseFeature(@RequestParam("ucode") ucode: String, @PathParam("featureId") featureId: Long): Boolean {
+        val feature = featureRepository.findById(featureId).get()
+        return feature.userAbleUseFeature(ucode)
     }
 }
