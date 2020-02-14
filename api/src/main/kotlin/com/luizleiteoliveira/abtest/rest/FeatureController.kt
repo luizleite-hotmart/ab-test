1878c02b15629bc42a7d756819aff2e7a450bfd2
Luiz Leite Oliveira <luiz.leite@hotmart.com>
2020-02-14 08:52:23 -0300

    improve imports

--- api/src/main/kotlin/com/luizleiteoliveira/abtest/rest/FeatureController.kt
+++ api/src/main/kotlin/com/luizleiteoliveira/abtest/rest/FeatureController.kt
@@ -2,10 +2,7 @@ package com.luizleiteoliveira.abtest.rest
 
 import com.luizleiteoliveira.abtest.FeatureRepository
 import com.luizleiteoliveira.abtest.entity.Feature
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.repository.support.Repositories
 import org.springframework.web.bind.annotation.*
-import javax.websocket.server.PathParam
 
 @RestController
 @RequestMapping("/features")
